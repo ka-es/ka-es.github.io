7c98a8b59b2a8ccb345cc8d1aad7010106657b1e
Jin-Xu <adam@lostwire.net>
2019-04-10 13:28:30 +0200

    Setting up basic skeleton for javascript methods

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+/* HTML library, this eventually will become a separate repository under
+ * lostwire organization on github */
 
+var html = {
+	'functions': {
+		'add_text': function(value) {
+			this.appendChild(document.createTextNode(value))
+			
+		}
+	}}
+html.common = {
+	'add_text': html.functions.add_text,
+}
+function div() {
+	return Object.assign(document.createElement('div'), html.common)
+}
+
+d = div()
+d.add_text('some text')
