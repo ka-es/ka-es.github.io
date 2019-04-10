1281125cd410624e456669c2cd3b4369d25af4f3
Jin-Xu <adam@lostwire.net>
2019-04-10 14:56:05 +0200

    Adding methods for managing html classes

--- js/main.js
+++ js/main.js
@@ -6,12 +6,68 @@ var html = {
 		'add_text': function(value) { this.appendChild(document.createTextNode(value)); },
 		'div': function(value) { return this.appendChild(html.assign_common(document.createElement('div'))); },
 	},
+	'elements': {
+		'a': function(href, label=null) {
+			var output = html.create_element('a');
+			output.setAttribute('href', href);
+			if (label) {
+				output.add_text(label);
+			}
+			return output;
+		},
+	},
+	'attribs': {
+		set: function(obj, name, value) {
+			if (obj.hasOwnProperty(name)) {
+				obj[name] = value;
+			} else {
+				obj.setAttribute(name, value);
+			}
+		},
+		get: function(obj, name) {
+			if (obj.hasOwnProperty(name)) {
+				return obj[name].bind(obj);
+			}
+			return obj.getAttribute(name);
+		},
+	},
 	'assign_common': function(element) { return Object.assign(element, html.common); },
+	'get_classes': function(element) {
+		var output = element.getAttribute('class');
+		return output ? output.split(' ') : new Array();
+	},
+	'add_class': function(str) {
+		var el_cls = html.get_classes(this);
+		var in_cls = str.split(' ');
+		for (var i=0; i<in_cls.length; i++) {
+			if (el_cls.indexOf(in_cls[i]) != null) {
+				el_cls.push(in_cls[i]);
+			}
+		}
+		this.setAttribute('class', el_cls.join(' '));
+	},
+	'create_element': function(name) {
+		var output = html.assign_common(document.createElement(name));
+		if (this) { this.appendChild(output); }
+		return output;
+	},
+	'remove_class': function(str) {
+		var el_cls = html.get_classes(this);
+		var in_cls = str.split(' ');
+		for (var i=0; i<in_cls.length; i++) {
+			var idx = el_cls.indexOf(in_cls[i]);
+			if (idx != null) {
+				el_cls.splice(idx, 1)
+			}
+		}
+		this.setAttribute('class', el_cls.join(' '));
+	},
 	'by_id': function(id) { return html.assign_common(document.getElementById(id)); },
 }
 html.common = {
 	'add_text': html.functions.add_text,
 	'div': html.functions.div,
+	'add_class': html.add_class,
+	'remove_class': html.remove_class,
+	'create_element': html.create_element,
 }
-
-html.by_id('main').div().add_text('costam')
